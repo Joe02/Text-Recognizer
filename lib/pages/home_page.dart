afb9f522f857ec8687e9d82eaf437370b6d828df
joe02 <joe02araujo@gmail.com>
2020-10-07 11:28:38 -0300

    Changes to main homepage.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter/rendering.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,106 +12,173 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+
+  File _image;
+  final picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            alignment: Alignment.center,
-            image: AssetImage('assets/images/app_background.png'),
-            colorFilter: ColorFilter.matrix([
-              0.393, 0.768, 0.189, 0.0, 0.0,
-              0.349, 0.686, 0.168, 0.0, 0.0,
-              0.272, 0.534, 0.131, 0.0, 0.0,
-              0.0, 0.0, 0.0, 0.2, 0.0,]),
-            fit: BoxFit.cover
-          ),
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.amberAccent,
-              Colors.orange,
-              Colors.orange,
-            ]
-          )
-        ),
-        child: Center(
-          child: Stack(
-            alignment: Alignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0, bottom: 100),
-                child: Opacity(
-                    opacity: 1,
-                    child: Transform.scale(
-                        scale: 0.5,
-                        child: Image.asset('assets/images/app_logo.png'))),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    alignment: Alignment.center,
+                    image: AssetImage('assets/images/app_bg.png'),
+                    colorFilter: ColorFilter.matrix([
+                      0.1,
+                      0.768,
+                      0.189,
+                      0.0,
+                      0.0,
+                      0.1,
+                      0.686,
+                      0.168,
+                      0.0,
+                      0.0,
+                      0.1,
+                      0.534,
+                      0.131,
+                      0.0,
+                      0.0,
+                      0.0,
+                      0.0,
+                      0.0,
+                      0.25 ,
+                      0.0,
+                    ]),
+                    fit: BoxFit.cover),
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.black,
+                      Colors.grey[900]
+                    ])),
+            child: Center(
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0, bottom: 50),
+                    child: Opacity(
+                        opacity: 0.2,
+                        child: Transform.scale(
+                            scale: 0.5,
+                            child: Image.asset(
+                              'assets/images/app_logo.png',
+                              color: Colors.grey[900],
+                            ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0, bottom: 50),
+                    child: Opacity(
+                        opacity: 1,
+                        child: Transform.scale(
+                            scale: 0.525,
+                            child: Image.asset(
+                              'assets/images/app_logo.png',
+                              color: Colors.white,
+                            ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 250.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        RaisedButton(
+                            onPressed: () {
+                              getImageFromGallery();
+                            },
+                            elevation: 7,
+                            color: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 3, right: 3),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Text(
+                                    "Galeria",
+                                    style: TextStyle(
+                                        color: Colors.grey[800], fontSize: 18),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 4.0),
+                                    child: Icon(
+                                      Icons.add_photo_alternate,
+                                      color: Colors.grey[800],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )),
+                        RaisedButton(
+                            onPressed: () {
+                              getImageFromCamera();
+                            },
+                            elevation: 7,
+                            color: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text(
+                                  "Câmera",
+                                  style: TextStyle(
+                                      color: Colors.grey[800], fontSize: 18),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 4.0),
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.grey[800],
+                                  ),
+                                )
+                              ],
+                            )),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 200.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    RaisedButton(
-                        onPressed: () {
-                          //TODO Open camera for recognition.
-                        },
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Text(
-                              "Adicionar",
-                              style: TextStyle(color: Colors.amber[500], fontSize: 18),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 4.0),
-                              child: Icon(
-                                Icons.add_photo_alternate,
-                                color: Colors.amber[500],
-                              ),
-                            )
-                          ],
-                        )),
-                    RaisedButton(
-                        onPressed: () {
-                          //TODO Navigate to Login Page
-                        },
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(left: 17.5),
-                              child: Text(
-                                "Login",
-                                style:
-                                    TextStyle(color: Colors.amber[500], fontSize: 18),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 4.0, right: 15),
-                              child: Icon(
-                                Icons.portrait,
-                                color: Colors.amber[500],
-                              ),
-                            )
-                          ],
-                        ))
-                  ],
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Future getImageFromCamera() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+
+        //TODO Navigate to recognition screen.
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future getImageFromGallery() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+
+        //TODO Navigate to recognition screen.
+      } else {
+        print('No image selected.');
+      }
+    });
   }
 }
